de86c3ea0e1f049c8bc5fe2973f6e1677407ac94
mholyaee <mh.olyaee@gmail.com>
2020-12-28 17:54:55 +0330

    Add files via upload

---
+++ S16_E4.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	char op;
+	int a,b;
+	bool flag=true;
+	while (flag)
+	{
+		cin>>op;
+		cin>>a>>b;
+		if (op=='/') cout<<a/b;
+		else if(op=='*') cout<<a*b;
+		else if (op=='+') cout<< a+b;
+		else if (op=='-') cout<<a-b;
+		else flag=false;
+	}
+}
